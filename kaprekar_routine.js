046e25cb3b80468b620f02975dca2540054a0e97
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-03-15 16:56:43 +0200

    refactoring kaprekar to take into account potential invalid user input
    not happy with current solution, will improve later

--- kaprekar_routine.js
+++ kaprekar_routine.js
@@ -1,8 +1,9 @@
 //kaprekar constant 6174 kaprekar routine
 
-/*helper function for kaprekar_routine function
-takes string of n, desired length of n, puts 0s in front of n
-so that n will be desired length but will not change its value as an int
+/*
+* helper function for kaprekar_routine function
+* takes string of n, desired length of n, puts 0s in front of n
+* so that n will be desired length but will not change its value as an int
 */
 function add_0s_front_of_n(stringn, desiredlength) {
     var num0 = desiredlength - stringn.length;
@@ -17,8 +18,8 @@ function add_0s_front_of_n(stringn, desiredlength) {
 }
 
 /*
-helper function for kaprekar_routine function
-takes a list of digits and returns a number composed of the elements of the list
+* helper function for kaprekar_routine function
+* takes a list of digits and returns a number composed of the elements of the list
 */
 function make_int_from_list(numberlist) {
     var newint = "";
@@ -28,33 +29,48 @@ function make_int_from_list(numberlist) {
         newint = newint.concat(numberlist[i].toString());
         i+=1;
     }
-    //console.log(typeof newint === 'string');
     newint = Number(newint);
-    //console.log(typeof newint === 'number');
     return newint;
 }
 
+/*
+* n should be an integer of 4 or fewer digits and not a repdigit (when it is 4 digits long
+* it can be supplied by the user as a repdigit of 3 or fewer digits)
+*/
+function verify_kaprekar_n_input(n, iterations) {
+    var strn = n.toString();
+    if ((iterations === 0) && ((typeof n !== 'number') || (! ('.' in strn)) || (strn.length > 4))) {
+        console.log("Input must be an integer of 4 or less digits in length");
+        return ["Input must be an integer of 4 or less digits in length",""];
+    }
+    else if (strn.length < 4) {
+        strn = add_0s_front_of_n(strn, 4);
+        return [Number(strn), strn];
+    }
+}
+/* Takes a positive integer n and returns how many iterations
+    it took to get to 6174 (Kaprekar's constant)
+    Input should be 4 or fewer digits and not a repdigit */
 function kaprekar_routine(n, iterations) {
-    //console.log(iterations);
+    //set 0 as default value of iterations
     iterations = typeof iterations !== 'undefined' ? iterations : 0;
-    //console.log(iterations);
+    //base case
     if (n===6174) {
         return iterations;
     }
     else {
-        var strn = n.toString();
-
+        //check that user has supplied valid initial input to function
+        narray = verify_kaprekar_n_input(n, iterations);
+        if (typeof narray[0] !== 'number') {
+            return narray[0];
+        }
+        var strn = narray[1];
+        n = narray[0];
     }
 }
 
 console.log(kaprekar_routine(6174));
 /*
-def kaprekar_routine(n, iterations=0):
-    """Takes a positive integer n and returns how many iterations
-    it took to get to 6174 (Kaprekar's constant)
-    Input should be 4 or fewer digits and not a repdigit"""
-    if n == 6174:
-        return iterations
     else:
         strn = str(n)
         if len(strn) < 4:
