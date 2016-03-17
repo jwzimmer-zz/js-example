76e6051befb6867bc2adf21d32c6c796b1581f2c
Aleruiz <ale@lattis.io>
2016-03-16 20:34:11 -0400

    module for export

--- kaprekar_routine.js
+++ kaprekar_routine.js
@@ -1,117 +1,126 @@
-//kaprekar constant 6174 kaprekar routine
+///kaprekar constant 6174 kaprekar routine
 
 /*
 * helper function for kaprekar_routine function
 * takes string of n, desired length of n, puts 0s in front of n
 * so that n will be desired length but will not change its value as an int
 */
-function add_0s_front_of_n(stringn, desiredlength) {
+module.exports = {
+  add_0s_front_of_n: function (stringn, desiredlength) {
     var num0 = desiredlength - stringn.length;
     var str0 = "";
     var i = 0;
     while (i<num0) {
-        str0 = str0.concat("0");
-        i += 1;
+      str0 = str0.concat("0");
+      i += 1;
     }
     stringn = str0.concat(stringn);
     return stringn;
-}
+  },
 
-/*
-* helper function for kaprekar_routine function
-* takes a list of digits and returns a number composed of the elements of the list
-*/
-function make_int_from_list(numberlist) {
+  /*
+   * helper function for kaprekar_routine function
+   * takes a list of digits and returns a number composed of the elements of the list
+   */
+  make_int_from_list: function(numberlist) {
     var newint = "";
     var i = 0;
     var max = numberlist.length;
     while (i<max) {
-        newint = newint.concat(numberlist[i].toString());
-        i+=1;
+      newint = newint.concat(numberlist[i].toString());
+      i+=1;
     }
     newint = Number(newint);
     return newint;
-}
+  },
 
-/*
-* n should be an integer of 4 or fewer digits and not a repdigit (when it is 4 digits long;
-* it can be supplied by the user as a repdigit of 3 or fewer digits)
-*/
-function verify_kaprekar_n_input(n, iterations) {
+  /*
+   * n should be an integer of 4 or fewer digits and not a repdigit (when it is 4 digits long;
+   * it can be supplied by the user as a repdigit of 3 or fewer digits)
+   */
+  verify_kaprekar_n_input: function(n, iterations) {
     var strn = n.toString();
     if ((iterations === 0) &&
         ((/^\+?\d+$/.test(strn) !== true) || (strn.length > 4))) {
-        return ["Input must be a positive integer of 4 or less digits in length",""];
+      return ["Input must be a positive integer of 4 or less digits in length",""];
     }
     else if ((strn.length < 4) && (Number(n) > 0)) {
-        n = Number(n);
-        strn = add_0s_front_of_n(strn, 4);
-        return [n, strn];
+      n = Number(n);
+      strn = module.exports.add_0s_front_of_n(strn, 4);
+      return [n, strn];
     }
     else {
-        //check that n is not a repdigit; by checking here instead of in the 1st if statement, I can rely on n
-        // to have the properties of a positive integer of length 4 so I can index into it
-        n = Number(n);
-        if (iterations === 0) {
-            if ((strn[0] !== strn[1]) || (strn[2] !== strn[3]) || (strn[1] !== strn[2]) && (n > 0)) {
-                return [n, strn];
-            }
-            else {
-                return ["Input must be a positive integer of 4 or less digits in length",""];
-            }
-        }
-        else if (n > 0) {
-            return [n, strn];
+      //check that n is not a repdigit; by checking here instead of in the 1st if statement, I can rely on n
+      // to have the properties of a positive integer of length 4 so I can index into it
+      n = Number(n);
+      if (iterations === 0) {
+        if ((strn[0] !== strn[1]) || (strn[2] !== strn[3]) || (strn[1] !== strn[2]) && (n > 0)) {
+          return [n, strn];
         }
         else {
-            return ["Input must be a positive integer of 4 or less digits in length",""];
+          return ["Input must be a positive integer of 4 or less digits in length",""];
         }
+      }
+      else if (n > 0) {
+        return [n, strn];
+      }
+      else {
+        return ["Input must be a positive integer of 4 or less digits in length",""];
+      }
     }
-}
-/* Takes a positive integer n and returns how many iterations
-    it took to get to 6174 (Kaprekar's constant)
-    Input should be 4 or fewer digits and not a repdigit */
-function kaprekar_routine(n, iterations) {
+  },
+  /* Takes a positive integer n and returns how many iterations
+   it took to get to 6174 (Kaprekar's constant)
+   Input should be 4 or fewer digits and not a repdigit */
+  kaprekar_routine: function(n, iterations) {
     //set 0 as default value of iterations
     iterations = typeof iterations !== 'undefined' ? iterations : 0;
     //since I am imagining a user and UI, user input would probably be interpreted as a string
     //(originally this function took a number as input)
     //so I need to convert the now expected "string" version of the input into a number
     //check that user has supplied valid initial input to function
-    var narray = verify_kaprekar_n_input(n, iterations);
+    var narray = module.exports.verify_kaprekar_n_input(n, iterations);
     n = narray[0];
     var strn = narray[1];
     //if the user input was not valid, return a message about invalid input
     if (typeof n !== 'number') {
-        return n;
+      return n;
     }
     //base case
     if (n===6174) {
-        return iterations;
+      return iterations;
     }
     else {
-        var nlist = [];
-        var j = 0;
-        while (j<strn.length) {
-            nlist.push(strn[j]);
-            j+=1;
-        }
-        //from small to large digits
-        nlist.sort(function(a,b){return a-b;});
-        var smalln = make_int_from_list(nlist);
-        //from large to small digits
-        nlist.reverse();
-        var largen = make_int_from_list(nlist);
-        var newn = largen - smalln;
-        iterations += 1;
-        //uncomment below line for debugging
-        //console.log(n, strn, newn, largen, smalln, iterations);
-        //return kaprekar_routine(newn, iterations);
-        return "hi";
+      var nlist = [];
+      var j = 0;
+      while (j<strn.length) {
+        nlist.push(strn[j]);
+        j+=1;
+      }
+      //from small to large digits
+      nlist.sort(function(a,b){return a-b;});
+      var smalln = module.exports.make_int_from_list(nlist);
+      //from large to small digits
+      nlist.reverse();
+      var largen = module.exports.make_int_from_list(nlist);
+      var newn = largen - smalln;
+      iterations += 1;
+      //uncomment below line for debugging
+      //console.log(n, strn, newn, largen, smalln, iterations);
+      return module.exports.kaprekar_routine(newn, iterations);
+      //you can use this line to intentionally trigger test failures in order to see how Jasmine works
+      //return "hi";
     }
-}
+  }
+};
 
-// Export node module
-if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
-    module.exports = kaprekar_routine;
-}
+// // Export node module
+// if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+//     module.exports = {
+//       kaprekar_routine: kaprekar_routine,
+//       add_0s_front_of_n: add_0s_front_of_n
+//       };
+// }
+// /*if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+//     module.exports = add_0s_front_of_n;
+// }*/
--- spec/testsSpec.js
+++ spec/testsSpec.js
@@ -1,7 +1,4 @@
 //note: I am imagining users accessing this function via a UI more or less like "enter number here: " with insructions.
-//I don't know how to prevent someone from entering a valid value for iterations and throwing off the result if they access the function directly
-//I can disallow non int values, negative values, and any values greater than 7, but for example if someone passed in an initial value 12, 1 then
-// the output would be 4 instead of 3, but since that is still less than 7 I am not sure how to flag it.
 
 var kaprekar_routine = require('../kaprekar_routine.js');
 
@@ -10,81 +7,106 @@ describe("Kaprekar Routine function", function() {
 		//sub categories: base case and recursive cases
 	describe("> Base Case n=6174", function() {
 	    it("should return 0 iterations", function() {
-	    	expect(kaprekar_routine(6174)).toEqual(0);
+	    	expect(kaprekar_routine.kaprekar_routine(6174)).toEqual(0);
 	    });
 
 	});
 	describe("> Recursive Cases", function() {
 		describe("> Recursive Case n=12, length 2,", function() {
 		    it("should return 3 iterations", function() {
-		    	expect(kaprekar_routine("12")).toEqual(3);
+		    	expect(kaprekar_routine.kaprekar_routine("12")).toEqual(3);
 		    });
 			it("should return the same value (3) with leading 0s in front of 12", function() {
-		    	expect(kaprekar_routine("0012")).toEqual(3);
+		    	expect(kaprekar_routine.kaprekar_routine("0012")).toEqual(3);
 		    });
 		});
 		describe("> Recursive Case n=111, length 3 and almost a repdigit,", function() {
 		    it("should return 5 iterations", function() {
-		    	expect(kaprekar_routine("111")).toEqual(5);
+		    	expect(kaprekar_routine.kaprekar_routine("111")).toEqual(5);
 		    });
 		});
 		describe("> Recursive Case n=1112, length 4,", function() {
 		    it("should return 5 iterations", function() {
-		    	expect(kaprekar_routine("1112")).toEqual(5);
+		    	expect(kaprekar_routine.kaprekar_routine("1112")).toEqual(5);
 		    });
 		});
 		describe("> Recursive Case n=1, length 1, minimum legal value,", function() {
 		    it("should return 5 iterations", function() {
-		    	expect(kaprekar_routine("1")).toEqual(5);
+		    	expect(kaprekar_routine.kaprekar_routine("1")).toEqual(5);
 		    });
 		});
 		describe("> Recursive Case n=9998, length 4, maximum legal value,", function() {
 		    it("should return 5 iterations", function() {
-		    	expect(kaprekar_routine("9998")).toEqual(5);
+		    	expect(kaprekar_routine.kaprekar_routine("9998")).toEqual(5);
 		    });
 		});
 	});
 	//testing failures, invalid input and the expected output
+	// sub case: special characters as invalid input
 	describe("> Invalid input to function", function() {
 		it("should return falure message if input is a negative number", function() {
-			expect(kaprekar_routine("-1")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("-1")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is a number that is 5 digits long", function() {
-			expect(kaprekar_routine("49857")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("49857")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is a number that is 5 digits long with a leading 0", function() {
 			//The kaprekar_routine function could be designed to handle this since 09857 = 9857 which fits the requirements for the routine
-			expect(kaprekar_routine("09857")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("09857")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is a number much longer than 4 digits", function() {
-			expect(kaprekar_routine("758473626254643828473")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("758473626254643828473")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is a decimal", function() {
-			expect(kaprekar_routine("48.8")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("48.8")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is a string", function() {
-			expect(kaprekar_routine("hello world")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("hello world")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is 0", function() {
-			expect(kaprekar_routine("0")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("0")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		it("should return falure message if input is a 4 digit repdigit", function() {
-			expect(kaprekar_routine("1111")).toEqual("Input must be a positive integer of 4 or less digits in length");
+			expect(kaprekar_routine.kaprekar_routine("1111")).toEqual("Input must be a positive integer of 4 or less digits in length");
 		});
 		describe("> Special Characters as invalid input to function", function() {
 			it("should return falure message if input has ! in it", function() {
-				expect(kaprekar_routine("48!")).toEqual("Input must be a positive integer of 4 or less digits in length");
+				expect(kaprekar_routine.kaprekar_routine("48!")).toEqual("Input must be a positive integer of 4 or less digits in length");
 			});
 			it("should return falure message if input has % in it", function() {
-				expect(kaprekar_routine("48%")).toEqual("Input must be a positive integer of 4 or less digits in length");
+				expect(kaprekar_routine.kaprekar_routine("48%")).toEqual("Input must be a positive integer of 4 or less digits in length");
 			});
 			it("should return falure message if input has < in it", function() {
-				expect(kaprekar_routine("<48")).toEqual("Input must be a positive integer of 4 or less digits in length");
+				expect(kaprekar_routine.kaprekar_routine("<48")).toEqual("Input must be a positive integer of 4 or less digits in length");
 			});
 			it("should return falure message if input has , in it", function() {
-				expect(kaprekar_routine("48,120")).toEqual("Input must be a positive integer of 4 or less digits in length");
+				expect(kaprekar_routine.kaprekar_routine("48,120")).toEqual("Input must be a positive integer of 4 or less digits in length");
 			});
 
 		});
+	});
+});
+
+describe("Kaprekar Routine helper functions", function() {
+	//testing the helper functions for kaprekar_routine
+		//sub categories: the individual functions
+	//since add_0s_front_of_n doesn't receive user input, it doesn't need to be tested for all kinds of crazy input
+	describe("> add_0s_front_of_n function", function() {
+	    it("should return n when n has length 4", function() {
+	    	expect(kaprekar_routine.add_0s_front_of_n("6174",4)).toEqual("6174");
+	    });
+	    it("should return 0+n when n has length 3", function() {
+	    	expect(kaprekar_routine.add_0s_front_of_n("174",4)).toEqual("0174");
+	    });
+	    it("should return 00+n when n has length 2", function() {
+	    	expect(kaprekar_routine.add_0s_front_of_n("74",4)).toEqual("0074");
+	    });
+	    it("should return 000+n when n has length 1", function() {
+	    	expect(kaprekar_routine.add_0s_front_of_n("4",4)).toEqual("0004");
+	    });
+	    it("should return 00000+n when n has length 1 and 6 is passed in as the length", function() {
+	    	expect(kaprekar_routine.add_0s_front_of_n("4",4)).toEqual("0004");
+	    });
+
 	});
 });
